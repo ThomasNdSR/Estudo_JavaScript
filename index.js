62fdbc5926b50818999f0a4ca477eb9618c2927a
Thomas Rodrigues <thomasnicola@outlook.com>
2022-03-31 11:56:01 -0300

    Campos Privados

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    _saldo;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.saldo = 0;
+ContaCorrenteRicardo.agencia = 1001;
