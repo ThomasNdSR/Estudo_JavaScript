b27bd243643b1b3a3e276d7aad2cf7e87c1efde9
Thomas Rodrigues <thomasnicola@outlook.com>
2022-04-02 12:06:39 -0300

    Sistema intero: Autenticacao, login e senha

--- Cliente.js
+++ Cliente.js
@@ -2,9 +2,12 @@ export class Cliente {
     get CPF(){
         return this._cpf;
     }
-
-    constructor(nome, cpf){
+    constructor(nome, cpf, senha){
         this.nome = nome;
         this._cpf = cpf
+        this._senha = senha;
+    }
+    autenticar(){
+        return false;
     }
 }
